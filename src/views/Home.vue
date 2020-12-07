ea3a7fd4ec4f0140cc3b847a1ca1180f385c3f3d
Jenniffer Crystine <jenniffercrystine@gmail.com>
2020-12-07 19:43:16 -0300

    homepage ready --missing interaction

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container class="home d-flex flex-column fill-height">
+    <div class="interaction d-flex flex-column justify-space-between fill-height">
+        <h1 class="text-h4 text-center">Sistema Informativo Alimentar</h1>
+        <div class="d-flex flex-column">
+          <v-btn class="login mt-2" rounded color="red lighten-1">Login</v-btn>    
+          <v-btn class="signin mt-2" rounded color="purple accent-1">Cadastra-se</v-btn>  
+        </div>
+        <a class="text-center grey--text align-center mt-2" href="#" >Iniciar sem cadastro</a>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    <h1 class="text-h4 text-center mt-5">Login</h1>
+    <v-form class="justify-center">
+        <v-text-field class="ml-20 mr-20" label="E-mail" :rules="rules" hide-details="auto"></v-text-field>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+};
+</script>
+
+<style scoped></style>
